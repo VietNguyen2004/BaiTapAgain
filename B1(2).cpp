7be3072453e241f50a7919a9381a3cd43fa0a1c6
VietNguyen2004 <85068521+VietNguyen2004@users.noreply.github.com>
2021-08-07 10:00:01 +0700

    Update B1(2).cpp

--- B1(2).cpp
+++ B1(2).cpp
@@ -1,14 +1,16 @@
+#include<iostream>
 #include<math.h>
 using namespace std;
 int a[1001];
 int tonguoc(int n)
 {
-    int result = 0;
+    int result = 1;
     int k = (int)sqrt(n);
-    for(int i = 1;i <= k ;i ++)
+    for(int i = 2;i <= k ;i ++)
     {
-        if(n % i == 0) result += i;
+        if(n % i == 0) result += i + n/i;
     }
+    if(k == sqrt(n)) result -= k;
     return result;
 }
 void taomang()
@@ -20,7 +22,7 @@ void xuat()
 {
     for(int i = 100;i <= 1000;i ++)
     {
-        for(int j = i+1;j <= 1000;j ++)
+        for(int j = i + 1;j <= 1000;j ++)
         {
             if(a[i] == j && a[j] == i) cout<<i<<" "<<j<<endl;
         }
